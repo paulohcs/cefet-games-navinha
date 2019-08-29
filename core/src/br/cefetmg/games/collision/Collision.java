f584ef0d69950cd1d7e42bd7d16be9d3c99a0c72
paulohcs <phsilva2309@gmail.com>
2019-08-29 12:27:47 -0300

    Collision

--- core/src/br/cefetmg/games/collision/Collision.java
+++ core/src/br/cefetmg/games/collision/Collision.java
@@ -2,6 +2,7 @@ package br.cefetmg.games.collision;
 
 import com.badlogic.gdx.math.Circle;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
 
 /**
  * Utilitário para verificação de colisão.
@@ -12,23 +13,40 @@ public class Collision {
 
     /**
      * Verifica se dois círculos em 2D estão colidindo.
+     *
      * @param c1 círculo 1
      * @param c2 círculo 2
      * @return true se há colisão ou false, do contrário.
      */
     public static final boolean circlesOverlap(Circle c1, Circle c2) {
-        return false;
+        Vector2 vetor = new Vector2(c1.x - c2.x, c1.y - c2.y);
+        /*
+        System.out.println(c1.x + " " + c1.y);
+        System.out.println(c2.x + " " + c2.y);
+        System.out.println(vetor.len());
+         */
+        if (vetor.len() <= c1.radius + c2.radius) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     /**
-     * Verifica se dois retângulos em 2D estão colidindo.
-     * Esta função pode verificar se o eixo X dos dois objetos está colidindo
-     * e então se o mesmo ocorre com o eixo Y.
+     * Verifica se dois retângulos em 2D estão colidindo. Esta função pode
+     * verificar se o eixo X dos dois objetos está colidindo e então se o mesmo
+     * ocorre com o eixo Y.
+     *
      * @param r1 retângulo 1
      * @param r2 retângulo 2
      * @return true se há colisão ou false, do contrário.
      */
     public static final boolean rectsOverlap(Rectangle r1, Rectangle r2) {
-        return false;
+        if ((r1.x + r1.width >= r2.x) && (r1.x <= r2.x + r2.width)
+                && (r1.y + r1.height >= r2.y) && (r1.y <= r2.y + r2.height)) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
